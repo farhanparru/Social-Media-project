78ea509c942132c0ae448f2d4c6363895fdaef09
Farhan <farhanparru87@gmail.com>
2024-05-24 00:39:01 -0500

    update login css

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -33,78 +33,91 @@ const Login = () => {
 
 
  return (
-  <div className="auth_page items-center justify-center">
-    <form onSubmit={handleSubmit}>
-      <div class="flex flex-wrap">
-        <div class="flex w-full flex-col md:w-1/2">
-          <div class="flex justify-center pt-12 md:-mb-24 md:justify-start md:pl-12">
-            <a href="#" class="border-b-gray-700 border-b-4 pb-2 text-2xl font-bold text-gray-900">Damasus.</a>
+  <div>
+    <section className="background-radial-gradient overflow-hidden">
+      <div className="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
+        <div className="row gx-lg-5 align-items-center mb-5">
+          <div className="col-lg-6 mb-5 mb-lg-0" style={{ zIndex: 10 }}>
+            <h1 className="my-5 display-5 fw-bold ls-tight" style={{ color: 'hsl(218, 81%, 95%)' }}>
+              The best offer <br />
+              <span style={{ color: 'hsl(218, 81%, 75%)' }}>for your business</span>
+            </h1>
+            <p className="mb-4 opacity-70" style={{ color: 'hsl(218, 81%, 85%)' }}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, expedita iusto veniam atque, magni tempora mollitia
+              dolorum consequatur nulla, neque debitis eos reprehenderit quasi ab ipsum nisi dolorem modi. Quos?
+            </p>
           </div>
-          <div class="lg:w-[28rem] mx-auto my-auto flex flex-col justify-center pt-8 md:justify-start md:px-6 md:pt-0">
-            <p class="text-left text-3xl font-bold italic hover:not-italic">Welcome back, Social Media App</p>
-            <p class="mt-2 text-left text-gray-500">Welcome back, please enter your details.</p>
-            <button class="-2 mt-8 flex items-center justify-center rounded-md border px-4 py-1 outline-none ring-gray-400 ring-offset-2 transition focus:ring-2 hover:border-transparent hover:bg-black hover:text-white">
-              <img class="mr-2 h-5" src="https://static.cdnlogo.com/logos/g/35/google-icon.svg" alt /> Log in with Google
-            </button>
-            <div class="relative mt-8 flex h-px place-items-center bg-gray-200">
-              <div class="absolute left-1/2 h-6 w-14 -translate-x-1/2 bg-white text-center text-sm text-gray-500">or</div>
-            </div>
-            <div class="flex flex-col pt-3 md:pt-8">
-              <div class="flex flex-col pt-4">
-                <div class="focus-within:border-b-gray-500 relative flex overflow-hidden border-b-2 transition">
-                  <input 
-                    class="w-full flex-1 appearance-none border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:outline-none" 
-                    placeholder="Email"
-                    type="email"
-                    onChange={handleChnageInput}
-                    value={email}
-                    name="email"
-                  />
-                </div>
+
+          <div className="col-lg-6 mb-5 mb-lg-0 position-relative">
+            <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
+            <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
+
+            <div className="card bg-glass">
+              <div className="card-body px-4 py-5 px-md-5">
+                <form>
+                  <div className="row">
+                    <div className="col-md-6 mb-4">
+                      <div className="form-outline">
+                        <input type="text" id="form3Example1" className="form-control" />
+                        <label className="form-label" htmlFor="form3Example1">First name</label>
+                      </div>
+                    </div>
+                    <div className="col-md-6 mb-4">
+                      <div className="form-outline">
+                        <input type="text" id="form3Example2" className="form-control" />
+                        <label className="form-label" htmlFor="form3Example2">Last name</label>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="form-outline mb-4">
+                    <input type="email" id="form3Example3" className="form-control" />
+                    <label className="form-label" htmlFor="form3Example3">Email address</label>
+                  </div>
+
+                  <div className="form-outline mb-4">
+                    <input type="password" id="form3Example4" className="form-control" />
+                    <label className="form-label" htmlFor="form3Example4">Password</label>
+                  </div>
+
+                  <div className="form-check d-flex justify-content-center mb-4">
+                    <input className="form-check-input me-2" type="checkbox" value="" id="form2Example33" defaultChecked />
+                    <label className="form-check-label" htmlFor="form2Example33">
+                      Subscribe to our newsletter
+                    </label>
+                  </div>
+
+                  <button type="submit" className="btn btn-primary btn-block mb-4">
+                    Sign up
+                  </button>
+
+                  <div className="text-center">
+                    <p>or sign up with:</p>
+                    <button type="button" className="btn btn-link btn-floating mx-1">
+                      <i className="fab fa-facebook-f"></i>
+                    </button>
+
+                    <button type="button" className="btn btn-link btn-floating mx-1">
+                      <i className="fab fa-google"></i>
+                    </button>
+
+                    <button type="button" className="btn btn-link btn-floating mx-1">
+                      <i className="fab fa-twitter"></i>
+                    </button>
+
+                    <button type="button" className="btn btn-link btn-floating mx-1">
+                      <i className="fab fa-github"></i>
+                    </button>
+                  </div>
+                </form>
               </div>
-              <div class="mb-12 flex flex-col pt-4">
-                <div class="focus-within:border-b-gray-500 relative flex overflow-hidden border-b-2 transition">
-                  <input 
-                    id="login-password" 
-                    class="w-full flex-1 appearance-none border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
-                    placeholder="Password" 
-                    type={typePass ? "text" : "password"}
-                    onChange={handleChnageInput}
-                    value={password}
-                    name="password"
-                  />
-                  <small
-                    className="absolute top-1/2 right-3 transform -translate-y-1/2 cursor-pointer"
-                    onClick={() => setTypePass(!typePass)}
-                  >
-                    {typePass ? <AiOutlineEyeInvisible className="w-6 h-6 ml-2" /> : <AiOutlineEye className="w-6 h-6 ml-2" />}
-                  </small>
-                </div>
-              </div>
-              <button type="submit" class="w-full rounded-lg bg-gray-900 px-4 py-2 text-center text-base font-semibold text-white shadow-md ring-gray-500 ring-offset-2 transition focus:ring-2">Log in</button>
-            </div>
-            <div class="py-12 text-center">
-              <p class="whitespace-nowrap text-gray-600">
-                Don't have an account?
-                <Link to="/register"> <a href="#" class="underline-offset-4 font-semibold text-gray-900 underline">Sign up for free.</a></Link>
-              </p>
             </div>
           </div>
-        </div>
-        <div class="pointer-events-none relative hidden h-screen select-none bg-black md:block md:w-1/2">
-          <div class="absolute bottom-0 z-10 px-8 text-white opacity-100">
-            <p class="mb-8 text-3xl font-semibold leading-10">.</p>
-            <p class="mb-4 text-3xl font-semibold"></p>
-            <p class=""></p>
-            <p class="mb-7 text-sm opacity-70"></p>
-          </div>
-          <img class="-z-1 absolute top-0 h-full w-full object-cover opacity-90" src="https://img.freepik.com/premium-vector/concept-illustration-man-woman-friends-having-online-conversation-messaging-chatting-communication-texting-messages-mobile-phone-apps-flat-cartoon-style_270158-412.jpg" />
         </div>
       </div>
-    </form>
+    </section>
   </div>
 );
-
 };
 
 export default Login;
