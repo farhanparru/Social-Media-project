7118c0b622b63459349e3709cdd9f0c41eb19fc7
Farhan <farhanparru87@gmail.com>
2024-05-24 01:50:29 -0500

    update final

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -34,87 +34,87 @@ const Login = () => {
 
  return (
   <div>
-    <section className="background-radial-gradient overflow-hidden">
-      <div className="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
-        <div className="row gx-lg-5 align-items-center mb-5">
-          <div className="col-lg-6 mb-5 mb-lg-0" style={{ zIndex: 10 }}>
-            <h1 className="my-5 display-5 fw-bold ls-tight" style={{ color: 'hsl(218, 81%, 95%)' }}>
-              The best offer <br />
-              <span style={{ color: 'hsl(218, 81%, 75%)' }}>for your business</span>
-            </h1>
-            <p className="mb-4 opacity-70" style={{ color: 'hsl(218, 81%, 85%)' }}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, expedita iusto veniam atque, magni tempora mollitia
-              dolorum consequatur nulla, neque debitis eos reprehenderit quasi ab ipsum nisi dolorem modi. Quos?
-            </p>
-          </div>
-
-          <div className="col-lg-6 mb-5 mb-lg-0 position-relative">
-            <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
-            <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
-
-            <div className="card bg-glass">
-              <div className="card-body px-4 py-5 px-md-5">
-                <form>
-                  <div className="row">
-                    <div className="col-md-6 mb-4">
-                      <div className="form-outline">
-                        <input type="text" id="form3Example1" className="form-control" />
-                        <label className="form-label" htmlFor="form3Example1">First name</label>
+    <section className="vh-100" style={{ backgroundColor: '#fee2e2' }}>
+      <form onSubmit={handleSubmit}>
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col col-xl-10">
+              <div className="card" style={{ borderRadius: '1rem' }}>
+                <div className="row g-0">
+                  <div className="col-md-6 col-lg-5 d-none d-md-block">
+                    <img
+                      src="https://img.freepik.com/premium-vector/concept-illustration-man-woman-friends-having-online-conversation-messaging-chatting-communication-texting-messages-mobile-phone-apps-flat-cartoon-style_270158-412.jpg"
+                      alt="login form"
+                      className="img-fluid"
+                      style={{ borderRadius: '1rem 0 0 1rem' }}
+                    />
+                  </div>
+                  <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                    <div className="card-body p-4 p-lg-5 text-black">
+                      <div className="d-flex align-items-center mb-3 pb-1">
+                        <i className="fas fa-cubes fa-2x me-3" style={{ color: '#ff6219' }}></i>
+                        <span className="h1 fw-bold mb-0">Logo</span>
                       </div>
-                    </div>
-                    <div className="col-md-6 mb-4">
-                      <div className="form-outline">
-                        <input type="text" id="form3Example2" className="form-control" />
-                        <label className="form-label" htmlFor="form3Example2">Last name</label>
+
+                      <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: '1px' }}>Sign into your account</h5>
+
+                      <div data-mdb-input-init className="form-outline mb-4">
+                        <input
+                          type="email"
+                          id="form2Example17"
+                          className="form-control form-control-lg"
+                          onChange={handleChnageInput}
+                          value={email}
+                          name="email"
+                        />
+                        <label className="form-label" htmlFor="form2Example17">Email address</label>
                       </div>
+
+                      <div className="form-outline mb-9 relative">
+                        <input
+                          type={typePass ? "text" : "password"}
+                          id="form2Example27"
+                          className="form-control form-control-lg pr-10"
+                          onChange={handleChnageInput}
+                          value={password}
+                          name="password"
+                        />
+                        <small
+                          className="absolute top-1/2 right-3 transform -translate-y-1/2 cursor-pointer"
+                          style={{ height: '44px' }}
+                          onClick={() => setTypePass(!typePass)}
+                        >
+                          {typePass ? (
+                            <AiOutlineEyeInvisible className="w-6 h-6" />
+                          ) : (
+                            <AiOutlineEye className="w-6 h-6" />
+                          )}
+                        </small>
+                        <label className="form-label" htmlFor="form2Example27">Password</label>
+                      </div>
+
+                      <div className="pt-1 mb-4">
+                        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                          Login
+                        </button>
+                      </div>
+
+                      <a className="small text-muted" href="#!">Forgot password?</a>
+                      <Link to="/register">
+                        <p className="mb-5 pb-lg-2" style={{ color: '#393f81' }}>
+                          Don't have an account? <a href="#!" style={{ color: '#393f81' }}>Register here</a>
+                        </p>
+                      </Link>
+                      <a href="#!" className="small text-muted">Terms of use.</a>
+                      <a href="#!" className="small text-muted">Privacy policy</a>
                     </div>
                   </div>
-
-                  <div className="form-outline mb-4">
-                    <input type="email" id="form3Example3" className="form-control" />
-                    <label className="form-label" htmlFor="form3Example3">Email address</label>
-                  </div>
-
-                  <div className="form-outline mb-4">
-                    <input type="password" id="form3Example4" className="form-control" />
-                    <label className="form-label" htmlFor="form3Example4">Password</label>
-                  </div>
-
-                  <div className="form-check d-flex justify-content-center mb-4">
-                    <input className="form-check-input me-2" type="checkbox" value="" id="form2Example33" defaultChecked />
-                    <label className="form-check-label" htmlFor="form2Example33">
-                      Subscribe to our newsletter
-                    </label>
-                  </div>
-
-                  <button type="submit" className="btn btn-primary btn-block mb-4">
-                    Sign up
-                  </button>
-
-                  <div className="text-center">
-                    <p>or sign up with:</p>
-                    <button type="button" className="btn btn-link btn-floating mx-1">
-                      <i className="fab fa-facebook-f"></i>
-                    </button>
-
-                    <button type="button" className="btn btn-link btn-floating mx-1">
-                      <i className="fab fa-google"></i>
-                    </button>
-
-                    <button type="button" className="btn btn-link btn-floating mx-1">
-                      <i className="fab fa-twitter"></i>
-                    </button>
-
-                    <button type="button" className="btn btn-link btn-floating mx-1">
-                      <i className="fab fa-github"></i>
-                    </button>
-                  </div>
-                </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </section>
   </div>
 );
